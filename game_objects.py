5bc452c315b0ec1cdd69518c5f3f58b48657b6a4
Mike Pool <mike.pool@pm.me>
2020-04-16 16:37:10 -0400

    Add initial project state

---
+++ game_objects.py
@@ -0,0 +1,63 @@
+import time
+#!!import random
+
+choices = {'rock': 0, 'paper': 1, 'scissors': 2)
+
+ROCK = 0
+PAPER = 1
+SCISSORS = 2
+
+def make_selection_int()
+		#!!logic to convert possible string input to corresponding integer value
+
+class game:
+	def __init__():
+		initialize_game()
+		
+	def initialize_game():
+		print('Welcome to Rock, Paper, Scissors!\n')
+		human = human_player()
+		computer = computer_player()
+		time.sleep(2)
+		human_name = input('Please enter a player name.\n')
+		human.set_name(human_name)
+	
+	def conduct_round():
+		human.set_selection(make_selection_int(input('Choose Rock, Paper, or Scissors.\n'')))
+		computer.make_selection()
+		winner_name = def resolve_shoot(human, computer):
+		print(f'{winner} won!')
+		
+	def resolve_shoot(player0, player1):
+		player0.get_selection()
+		player1.get_selection()
+		#!!logic to determine winner
+		return winner
+		
+	def end_game():
+		name = human.get_name()
+		print(f'Thanks for playing {name}!')
+		
+	
+class player:
+	def set_selection(selection):
+		selection = selection
+		
+	def get_selection():
+		return selection
+	
+	def set_name(name):
+		name = str(name)
+	
+	def get_name
+		return name	
+
+class (player)computer_player:
+	def __init__():
+		set_name('computer')
+	
+	def make_selection():
+		set_selection(#!!get random number 0, 1, or 2)
+		
+class (player)human_player:
+	
