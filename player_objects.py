b8240eb09112100457ca6e946bf67b17487afad5
Michael Pool <POOLMIC@wuh-intern.de>
2020-04-18 14:17:36 -0400

    Fix issue with computer player makek_selection function

--- player_objects.py
+++ player_objects.py
@@ -10,14 +10,12 @@ class player:
 	@property
 	def selection(self):
 		return self._selection
-		print('getting')
 
 	@selection.setter
 	def selection(self, value):
-		self._selection = choices[0]
-		print('setting ' + choices[0])
+		self._selection = choices[value]
 		#!!handle string input
-		
+	
 class human_player(player):
 	def __init__(self):			
 		super().__init__()
@@ -29,15 +27,18 @@ class computer_player(player):
 		self.name = 'The computer'
 	
 	def make_selection(self):
-		self._selection = player.selection.fset(self, random.randint(0,2))
-		print(self.selection)
+		player.selection.fset(self, random.randint(0,2))
+		#super().selection.fset(self, random.randint(0,2)) why isn't this the same as above?
 		
 
 if __name__ == '__main__':
 	human = human_player()
 	human.selection = 1
-	print(human.selection) #why no 'getting' printed?
-
+	print('HUMAN')
+	print(human.selection)
+	print()
+	
+	print('COMP')
 	comp = computer_player()
 	comp.make_selection()
 	print(comp.selection) #why none?
